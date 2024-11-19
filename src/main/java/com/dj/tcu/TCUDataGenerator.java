4a7c724be9dfc2fcde38e6e92621bfbf0cd5ee89
TheDJGamerYT <156543381+TheDJGamerYT@users.noreply.github.com>
2024-11-19 17:22:50 -0600

    chicken nugg

---
+++ src/main/java/com/dj/tcu/TCUDataGenerator.java
@@ -0,0 +1,11 @@
+package com.dj.tcu;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TCUDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
